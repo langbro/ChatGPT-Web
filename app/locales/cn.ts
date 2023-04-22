b174ccc5d494136a33b817974542887b959bc841
langbro <130909856+langbro@users.noreply.github.com>
2023-04-22 11:19:10 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -89,7 +89,7 @@ const cn = {
       IsLatest: "已是最新版本",
       CheckUpdate: "检查更新",
       IsChecking: "正在检查更新...",
-      FoundUpdate: (x: string) => `：${x}`,
+      FoundUpdate: (x: string) => ``,
       GoToUpdate: "",
     },
     SendKey: "发送键",
